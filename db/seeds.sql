4108923f12af54ddcaa49a8ef90e5d288e7e03f3
Alan Wagner <alanwagner101@gmail.com>
2018-12-28 13:01:25 -0600

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Jucy Lucy", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cowboy Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("California Burger", false);
---
+++ models/burger.js
@@ -0,0 +1,15 @@
+var orm = require("../config/orm");
+
+var burger = {
+    all: function() {
+
+    },
+    create: function() {
+
+    },
+    update: function() {
+
+    }
+};
+
+module.exports = burger;
